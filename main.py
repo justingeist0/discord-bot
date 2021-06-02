eaf0b269c962f1604ebb97e43f4f2bbbd4723cad
Justin Geist <justin.geist@inmar.com>
2021-06-02 19:42:23 -0400

    Update main.py
    Pig v2

--- main.py
+++ main.py
@@ -1,182 +1,309 @@
-import discord
-import random
 import os
+from discord import Intents
+from discord.ext import tasks
+from discord.ext import commands
+from discord.ext.commands import Context
+from discord.utils import get
 
-token = os.getenv("DISCORD_BOT_TOKEN")
+TOKEN: str = os.getenv("DISCORD_BOT_TOKEN")
 
-client = discord.Client()
+COMMAND_PREFIX = '!'
+INTENTS = Intents.default()
+INTENTS.members = True
+BOT = commands.Bot(command_prefix='!', intents=INTENTS)
 
-reaction = '💺'
-wait_list_instructions = "React with " + reaction + " to join %s's wait list.\n" \
-                       "*Remove reaction when you are no longer waiting.*"
-reserve_wait_list = "React with " + reaction + " to reserve a seat at %s's upcoming game.\n" \
-                       "*Remove reaction to cancel reservation.*"
-empty_str = '\n*No one waiting yet*'
-mention_str = '<@!%s>'
-new_line_mention_str = '\n<@!%s>'
-active_games = 'active-games'
-poker_now_link = 'pokernow.club'
+WL_COMMAND = 'wl'
+RESERVE_COMMAND = 'reserve'
+INTEREST_COMMAND = 'interest'
+MY_CASH_APPS_COMMAND = 'mycashapps'
+PING_COMMAND = 'ping'
+HELP_COMMAND = 'pighelp'
+CLEAR_COMMAND = 'clear'
+TOGGLE_AUTO_POST_COMMAND = 'toggle'
+RANK_REQUEST_COMMAND = 'rolerequest'
+NO_AUTO_POST_USER_ID = set()
+ACTIVE_GAMES_CHANNEL = 'active-games'
+CASH_APPS_CHANNEL = 'cashapp-and-venmos'
+HELP_MESSAGE_HOST = \
+    '*Poker Pig Help:*\n'\
+    '**%s**: Show help message.' % (COMMAND_PREFIX + HELP_COMMAND) + '\n'\
+    '**%s**: Ping poker pig.' % (COMMAND_PREFIX + PING_COMMAND) + '\n'\
+    '**%s**: Fetch all your images from cashapp-and-venmos.' % (COMMAND_PREFIX + MY_CASH_APPS_COMMAND) + '\n' \
+    '**%s**: Create a wait list.' % (COMMAND_PREFIX + WL_COMMAND) + '\n' \
+    '**%s**: Create a reservation wait list.' % (COMMAND_PREFIX + RESERVE_COMMAND) + '\n' \
+    '**%s**: Toggle auto call !wl & !mycashapps in the channel %s' % ((COMMAND_PREFIX + TOGGLE_AUTO_POST_COMMAND), ACTIVE_GAMES_CHANNEL) + '\n'\
+    '**%s** *optional*@{Someone}: Delete all messages from yourself or someone else in active-games.' % (COMMAND_PREFIX + CLEAR_COMMAND) + '\n'\
+    '\nAdmin only:\n'\
+    '**%s** @{Role}: Create a message to request a rank.' % (COMMAND_PREFIX + RANK_REQUEST_COMMAND) + '\n'\
+    '**%s** {Game Description}: Create a wait list to find interested players.' % (COMMAND_PREFIX + INTEREST_COMMAND)
+    #'*Poker Pig last updated on 3/03/2021, 6/2/2021*'
+MENTION_STR = '<@!%s>'
+WAIT_LIST_EMPTY_STR = '*No one*'
+POKER_NOW_LINK = 'pokernow.club'
+WAIT_EMOJI = '💺'
+RANK_REQUEST_EMOJI = '👋'
 
-wl_command = '!wl'.lower()
-reserve_command = '!reserve'.lower()
-money_command = '!buyin'.lower()
-ignore_command = '!botignore'
-help_command = '!pighelp'
-clear_command = '!clear'
-help_message = '*Poker Pig Commands:*\n'\
-               '**%s**: Show help message.' % help_command + '\n'\
-               '**%s**: Create a reserve wait list.' % reserve_command + '\n' \
-               '**%s**: Create a wait list.' % wl_command + '\n' \
-               '**%s**: Fetch all your images from cashapp-and-venmos.' % money_command + '\n' \
-               '**%s**: Delete all messages related to your game in active-games.' % clear_command + '\n\n' \
-               'When a game link is posted in the active-games(or bot-testing) channel, **%s** and **%s** are called automatically.' % (money_command, wl_command) + '\n\n' \
-               '*Poker Pig last updated on 3/03/2021*'
+INTERESTED_PLAYERS = set()
 
 
-@client.event
+class InterestedPlayer:
+    def __init__(self, user_id, channel_id, wl_message_id):
+        self.user_id = user_id
+        self.channel_id = channel_id
+        self.wl_message_id = wl_message_id
+        self.minutes_left = 60
+        print("created interested player")
+
+    def tick_expired(self):
+        self.minutes_left -= 1
+        print(self.minutes_left)
+        return self.minutes_left <= 0
+
+
+@tasks.loop(minutes=1)
+async def tick_every_minute():
+    expired = set(ip for ip in INTERESTED_PLAYERS if ip.tick_expired())
+    for e in expired:
+        await remove_from_wait_list(
+            await BOT.get_channel(e.channel_id).fetch_message(e.wl_message_id),
+            e.user_id
+        )
+
+
+@BOT.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(BOT))
+    tick_every_minute.start()
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        if str(message.content).find(reaction) != -1:
-            await message.add_reaction(reaction)
-        return
-    user_id = message.author.id
-    lower_case_message = str(message.content).lower()
-    if lower_case_message.find(ignore_command) != -1:
-        return
-    elif lower_case_message == 'ping':
-        await message.channel.send('Pong ' + ('ping! ' if bool(random.getrandbits(1)) else ' ') + '(' + str(round(client.latency * 100)) + 'ms)')
+def can_host(ctx):
+    for role in ctx.message.author.roles:
+        if True in (host in str(role).lower() for host in ('host', 'floor', 'admin')):
+            return True
 
-    elif lower_case_message == 'pong':
-        await message.channel.send('Ping' + (' pong!' if bool(random.getrandbits(1)) else ''))
 
-    elif lower_case_message == 'beep':
-        await message.channel.send('Boop' + (' beep!' if bool(random.getrandbits(1)) else ''))
+def is_admin(ctx):
+    for role in ctx.message.author.roles:
+        if True in (host in str(role).lower() for host in ('floor', 'admin')):
+            return True
 
-    elif lower_case_message == 'boop':
-        await message.channel.send('Beep' + (' boop!' if bool(random.getrandbits(1)) else ''))
 
-    elif lower_case_message == 'achoo':
-        await message.channel.send('Bless you!' if bool(random.getrandbits(1)) else 'Cover your mouth!')
+@BOT.command(name=PING_COMMAND)
+async def ping(ctx: Context):
+    await ctx.message.reply('Pong! (' + str(round(BOT.latency * 100)) + 'ms)')
 
-    elif help_command == lower_case_message:
-        await message.channel.send(help_message)
 
-    elif clear_command == lower_case_message:
-        await clear_messages(message)
+@BOT.command(name=HELP_COMMAND)
+async def poker_pig_help(ctx: Context):
+    if can_host(ctx):
+        await ctx.message.reply(HELP_MESSAGE_HOST)
 
-    elif wl_command == lower_case_message:
-        await start_wait_list(message, user_id, False)
 
-    elif reserve_command == lower_case_message:
-        await start_wait_list(message, user_id, True)
+async def start_wait_list(ctx, header_str: str, footer_str: str):
+    message_to_send = header_str + ("\n%s\n\n" % WAIT_LIST_EMPTY_STR) + footer_str
+    sent = await ctx.message.channel.send(message_to_send)
+    await sent.add_reaction(WAIT_EMOJI)
 
-    elif money_command == lower_case_message:
-        await show_supported_payments(message, user_id)
 
-    elif lower_case_message.find(poker_now_link) != -1:
-        if active_games in str(message.channel) or 'bot-testing' in str(message.channel):
-            await start_wait_list(message, user_id, False)
-            await show_supported_payments(message, user_id)
+@BOT.command(name=WL_COMMAND)
+async def wl(ctx: Context):
+    if can_host(ctx):
+        await start_wait_list(
+            ctx,
+            "**Wait List | Host %s:**" % (MENTION_STR % ctx.author.id),
+            "React with :seat: to be added"
+        )
+
+
+@BOT.command(name=RESERVE_COMMAND)
+async def reserve(ctx: Context):
+    if can_host(ctx):
+        await start_wait_list(
+            ctx,
+            "**Reserved Players | Host %s:**" % (MENTION_STR % ctx.author.id),
+            "React with :seat: to reserve"
+        )
+
+
+def get_channel(ctx: Context, channel_substr: str):
+    for channel in ctx.message.guild.text_channels:
+        if channel_substr in str(channel):
+            return channel
+
+
+async def show_supported_payments(ctx, user_id: str):
+    messages = await get_channel(ctx, CASH_APPS_CHANNEL).history(limit=1000).flatten()
+    image_links = []
+    for m in messages:
+        if user_id == str(m.author.id):
+            for image in m.attachments:
+                image_links.append(str(image.url))
+    text_to_send = ":moneybag:" + MENTION_STR % user_id + ":moneybag:"
+    if len(image_links) == 0:
+        text_to_send += '\nDM host for their money apps.'
+    header = await ctx.message.channel.send(text_to_send)
+    for link in image_links:
+        await header.reply(link)
+
+
+@BOT.command(name=MY_CASH_APPS_COMMAND)
+async def cash_apps(ctx: Context):
+    if can_host(ctx):
+        await show_supported_payments(ctx, str(ctx.author.id))
+
+
+@BOT.command(name=TOGGLE_AUTO_POST_COMMAND)
+async def toggle(ctx: Context):
+    if can_host(ctx):
+        user_id = str(ctx.author.id)
+        if user_id in NO_AUTO_POST_USER_ID:
+            NO_AUTO_POST_USER_ID.remove(user_id)
+            await ctx.message.reply("Auto post enabled.")
         else:
-            for r in message.author.roles:
-                if "host" in str(r).lower():
-                    return
-            await message.reply(mention_str % user_id + " Bad man, Bad man! you are not authorized to post games here!")
+            NO_AUTO_POST_USER_ID.add(user_id)
+            await ctx.message.reply("Auto post disabled.")
+
+
+@BOT.event
+async def on_message(message):
+    if POKER_NOW_LINK in str(message.content).lower():
+        user_id = str(message.author.id)
+        ctx = await BOT.get_context(message)
+        if True in (s in str(message.channel).lower() for s in (ACTIVE_GAMES_CHANNEL, 'bot-testing')) \
+                and user_id not in NO_AUTO_POST_USER_ID:
+            await wl(ctx)
+            await cash_apps(ctx)
+        elif not can_host(ctx):
+            await message.reply(MENTION_STR % user_id + " Bad man, Bad man! you are not authorized to post games here!")
             await message.delete()
+    else:
+        await BOT.process_commands(message)
 
 
-async def show_supported_payments(message, user_id):
-    payment_links = [":moneybag:" + mention_str % user_id + ":moneybag:\n"
-                                 "*Enter your table name for each payment and request.*\n"]
-    for channel in message.guild.text_channels:
-        if "cashapp-and-venmos" in str(channel):
-            messages = await channel.history(limit=500).flatten()
-            for m in messages:
-                if str(message.author) == str(m.author):
-                    for image in m.attachments:
-                        payment_links.append(image.url)
-            break
-    if len(payment_links) == 1:
-        payment_links.append('**No images found.**')
-    for i in payment_links:
-        await message.channel.send(i)
-
-
-async def start_wait_list(message, name_id, is_reserve_wait_list):
-    header_str = "**Line up:**" if is_reserve_wait_list else "**Wait List:**"
-    message_to_send = header_str + \
-                      "%s\n\n" % empty_str + (reserve_wait_list % (mention_str % name_id) if is_reserve_wait_list
-                        else wait_list_instructions % (mention_str % name_id))
-    await message.channel.send(message_to_send)
-
-
-async def clear_messages(message):
-    if active_games in str(message.channel) or 'bot-testing' in str(message.channel):
-        messages = await message.channel.history(limit=100).flatten()
-        user = message.author
-        delete_next_if_bot = False
-        reversed_messages = []
+async def clear_messages(message, user_id: str):
+    if ACTIVE_GAMES_CHANNEL in str(message.channel) or 'bot-testing' in str(message.channel):
+        messages = (await message.channel.history(limit=100).flatten())[::-1]
+        message_id = []
         for m in messages:
-            reversed_messages.insert(0, m)
-        for m in reversed_messages:
-            if str(user) == str(m.author):
-                await m.delete()
-                delete_next_if_bot = True
-            elif m.author == client.user and delete_next_if_bot:
-                await m.delete()
-            else:
-                delete_next_if_bot = False
+            try:
+                if str(m.author.id) in user_id or user_id in str(m.content)\
+                        or m.reference.message_id in message_id:
+                    message_id.append(m.id)
+                    await m.delete()
+            except AttributeError:
+                continue
 
 
-@client.event
-async def on_raw_reaction_add(payload):
-    if payload.user_id == client.user.id or str(payload.emoji) != reaction:
-        return
-    print('Added reaction' + str(payload))
-    message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
-    await add_to_wait_list(message, new_line_mention_str % payload.user_id)
+@BOT.command(name=CLEAR_COMMAND)
+async def clear(ctx: Context, user_id: str = None):
+    if user_id is None:
+        user_id = str(ctx.message.author.id)
+    if can_host(ctx):
+        await clear_messages(ctx.message, user_id)
 
 
-async def add_to_wait_list(message, mention_user):
-    if message.author == client.user and str(message.content).find(reaction) != -1:
-        wait_list_str = str(message.content)
-        wait_list_str = wait_list_str.replace(empty_str, "")
+def add_to_interested_players(wl_message, user_id):
+    if 'interest' in str(wl_message.content).lower():
+        INTERESTED_PLAYERS.add(InterestedPlayer(
+            user_id,
+            wl_message.channel.id,
+            wl_message.id)
+        )
+
+
+async def add_to_wait_list(wl_message, user_id):
+    wait_list_str = str(wl_message.content)
+    wait_list_str = wait_list_str.replace('\n' + WAIT_LIST_EMPTY_STR, "")
+    first_line_end_idx = wait_list_str.find('\n')
+    if user_id not in wait_list_str[first_line_end_idx:]:
         start_idx = wait_list_str.find('\n\n')
-        if wait_list_str.find(mention_user) != -1:
-            return
-        wait_list_str = wait_list_str[0:start_idx] + mention_user + wait_list_str[start_idx:]
-        wait_list_str = remove_numbers_before_mentions(wait_list_str)
-        await message.edit(content=add_numbers_before_mentions(wait_list_str))
+        wait_list_str = wait_list_str[:start_idx] + '\n' + MENTION_STR % user_id + wait_list_str[start_idx:]
+        add_to_interested_players(wl_message, user_id)
+        await wl_message.edit(content=wait_list_str)
 
 
-def remove_numbers_before_mentions(waist_list_str):
-    return waist_list_str
+@BOT.command(name=RANK_REQUEST_COMMAND)
+async def rank_request(ctx: Context, role):
+    if is_admin(ctx):
+        sent = await ctx.channel.send("React with " + RANK_REQUEST_EMOJI + " to be given/removed from " + role + " role")
+        await sent.add_reaction(RANK_REQUEST_EMOJI)
 
 
-def add_numbers_before_mentions(waist_list_str):
-    return waist_list_str
+async def give_player_rank(member, rank_request_message):
+    text = str(rank_request_message.content)
+    role_id = int(text[text.find('<@&')+3:text.find('>')])
+    role = get(rank_request_message.guild.roles, id=role_id)
+    await member.add_roles(role)
 
 
-@client.event
+@BOT.event
+async def on_raw_reaction_add(payload):
+    emoji = str(payload.emoji)
+    if payload.user_id == BOT.user.id or emoji not in (WAIT_EMOJI, RANK_REQUEST_EMOJI):
+        return
+    message = await BOT.get_channel(payload.channel_id).fetch_message(payload.message_id)
+    if message.author.id == BOT.user.id:
+        if emoji == WAIT_EMOJI:
+            await add_to_wait_list(message, str(payload.user_id))
+        elif emoji == RANK_REQUEST_EMOJI:
+            await give_player_rank(payload.member, message)
+
+
+async def remove_from_interested_players(wl_message, user_id):
+    if 'interest' in str(wl_message.content).lower():
+        for ip in INTERESTED_PLAYERS:
+            if ip.user_id == user_id and ip.wl_message_id == wl_message.id:
+                INTERESTED_PLAYERS.remove(ip)
+                await wl_message.remove_reaction(WAIT_EMOJI, await BOT.fetch_user(user_id))
+                break
+
+
+async def remove_from_wait_list(wl_message, user_id):
+    wait_list_str = str(wl_message.content)
+    if user_id in wait_list_str:
+        first_line_end_idx = wait_list_str.find('\n')
+        wait_list_str = wait_list_str[:first_line_end_idx] + wait_list_str[first_line_end_idx:]\
+            .replace('\n' + MENTION_STR % user_id, '')
+        is_empty = '<@!' not in wait_list_str[first_line_end_idx:]
+        if is_empty:
+            wait_list_str = wait_list_str[:first_line_end_idx] + '\n' \
+                            + WAIT_LIST_EMPTY_STR + wait_list_str[first_line_end_idx:]
+        await wl_message.edit(content=wait_list_str)
+        await remove_from_interested_players(wl_message, user_id)
+
+
+async def remove_player_rank(payload, rank_request_message):
+    text = str(rank_request_message.content)
+    role_id = int(text[text.find('<@&')+3:text.find('>')])
+    guild = await BOT.fetch_guild(payload.guild_id)
+    role = get(guild.roles, id=role_id)
+    member = await guild.fetch_member(payload.user_id)
+    await member.remove_roles(role)
+
+
+@BOT.event
 async def on_raw_reaction_remove(payload):
-    if str(payload.emoji) == reaction:
-        print('Removed reaction' + str(payload))
-        message = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
-        if message.author == client.user:
-            wait_list_str = str(message.content)
-            mention_user = new_line_mention_str % payload.user_id
-            if wait_list_str.find(mention_user) != -1:
-                wait_list_str = wait_list_str.replace(mention_user, '')
-                name_idx = wait_list_str.find('<@!')
-                if reaction in wait_list_str[0:name_idx]:
-                    start_idx = wait_list_str.find('\n\n')
-                    wait_list_str = wait_list_str[0:start_idx] + empty_str + wait_list_str[start_idx:]
-                await message.edit(content=wait_list_str)
+    emoji = str(payload.emoji)
+    if payload.user_id == BOT.user.id or emoji not in (WAIT_EMOJI, RANK_REQUEST_EMOJI):
+        return
+    message = await BOT.get_channel(payload.channel_id).fetch_message(payload.message_id)
+    if message.author.id == BOT.user.id:
+        if emoji == WAIT_EMOJI:
+            await remove_from_wait_list(message, str(payload.user_id))
+        elif emoji == RANK_REQUEST_EMOJI:
+            await remove_player_rank(payload, message)
 
 
-client.run(token)
+@BOT.command(name=INTEREST_COMMAND)
+async def interest(ctx: Context, *args):
+    if is_admin(ctx):
+        await start_wait_list(
+            ctx,
+            "**Interest in %s**" % " ".join(args),
+            "React with :seat: to be added\n"
+            "*Your reaction is removed after one hour*"
+        )
+        await ctx.message.delete()
+
+
+BOT.run(TOKEN)
